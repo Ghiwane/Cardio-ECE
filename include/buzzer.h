64825c96db803da452adfd488a880dabfe03bc5c
Ghiwane <sehaki.ghiwane@gmail.com>
2026-09-26 20:03:52 +0200

    Add function declarations for buzzer management and button state detection

--- include/buzzer.h
+++ include/buzzer.h
@@ -1,7 +1,8 @@
 #ifndef BUZZER_H
 #define BUZZER_H
 
-void buzzerInit();
-void gererBeeper(float bpm);
+void buzzerInit(); // initialisation du buzzer
+void gererBuzzer(float bpm); // gestion des tonnalités du buzzer
+void etatBuzzer(int etatBouton); // detection d'appuie du bouton pour stopper ou reprendre les beep
 
 #endif
